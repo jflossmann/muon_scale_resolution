7d9ad603c100ef9b1134acc95d64dbe908f60333
dguthmann98 <73468537+dguthmann98@users.noreply.github.com>
2023-09-18 13:40:30 +0200

    Update roch.py

--- roch.py
+++ roch.py
@@ -1,1 +1,16 @@
+import ROOT
 
+def rochester():
+  ROOT.gROOT.Reset()
+  ROOT.gROOT.SetBatch()
+  ROOT.gStyle.SetOptStat(0)
+
+  Anno_2018=True
+  lumi=86.6855
+  if Anno_2018: lumi=373
+
+  reweight=1
+
+  crossSektion=13730 #pb (from DAS)
+
+  
