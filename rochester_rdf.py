ad1f5875693c13ad054d5801083dbca615bf7fa8
jdriesch <jost.driesch@kit.edu>
2023-09-27 19:35:52 +0200

    added one over pT hists

--- rochester_rdf.py
+++ rochester_rdf.py
@@ -5,7 +5,7 @@ from array import array
 
 # function in c++ code. Finds indices of muons closest to z boson mass
 ROOT.gInterpreter.Declare("""
-    ROOT::VecOps::RVec<Int_t> get_indices(UInt_t nMuon, ROOT::VecOps::RVec<Float_t> *Muon_pt, ROOT::VecOps::RVec<Float_t> *Muon_eta, ROOT::VecOps::RVec<Float_t> *Muon_phi, ROOT::VecOps::RVec<Float_t> *Muon_mass, ROOT::VecOps::RVec<Float_t> *Muon_tkRelIso, ROOT::VecOps::RVec<Bool_t> *Muon_mediumId){
+    ROOT::VecOps::RVec<Int_t> get_indices(UInt_t nMuon, ROOT::VecOps::RVec<Float_t> *Muon_pt, ROOT::VecOps::RVec<Float_t> *Muon_eta, ROOT::VecOps::RVec<Float_t> *Muon_phi, ROOT::VecOps::RVec<Float_t> *Muon_mass, ROOT::VecOps::RVec<Float_t> *Muon_tkRelIso, ROOT::VecOps::RVec<Bool_t> *Muon_mediumId, ROOT::VecOps::RVec<Int_t> *Muon_charge){
         Float_t deltaM = 1000;
         Int_t ind1, ind2;
         ind1 = -99;
@@ -21,18 +21,26 @@ ROOT.gInterpreter.Declare("""
                 if(fabs(Muon_eta->at(j) > 2.4)) continue;
                 if(Muon_tkRelIso->at(j) > 0.1) continue;
                 if(Muon_mediumId->at(j)==0) continue;
-            
+                if(Muon_charge->at(i) * Muon_charge->at(j) > 0) continue;
+
                 TLorentzVector mui, muj, Z;
                 mui.SetPtEtaPhiM(Muon_pt->at(i), Muon_eta->at(i), Muon_phi->at(i), Muon_mass->at(i));
                 muj.SetPtEtaPhiM(Muon_pt->at(j), Muon_eta->at(j), Muon_phi->at(j), Muon_mass->at(j));
                 Z = mui + muj;
                 if (fabs(Z.M() - 91.1876) < deltaM){
                     deltaM = fabs(Z.M() - 91.1876);
-                    ind1 = i;
-                    ind2 = j;
+                    if (Muon_charge->at(i) < 0){
+                        ind1 = i;
+                        ind2 = j;
+                    }
+                    else {
+                        ind1 = j;
+                        ind2 = i;
+                    }
                 }
             }
         }
+
         ROOT::VecOps::RVec<Int_t> s(2);
         s[0] = ind1;
         s[1] = ind2;
@@ -94,27 +102,27 @@ ROOT.gInterpreter.Declare(
 )
 
 
-def make_ntuples(samples):
-    datadir = "/ceph/jdriesch/rochester/"
+def make_ntuples(nanoAODs, ntuples):
 
-    for s in samples[:2]:
+    for s in nanoAODs:
         quants = [
             "pt_Z", "mass_Z", "eta_Z", "phi_Z",
-            "pt_1", "mass_1", "eta_1", "phi_1",
-            "pt_2", "mass_2", "eta_2", "phi_2",
+            "pt_1", "mass_1", "eta_1", "phi_1", "charge_1",
+            "pt_2", "mass_2", "eta_2", "phi_2", "charge_2",
         ]
         # load nanoAOD
-        rdf = ROOT.RDataFrame("Events", f"{datadir}/{s}.root")
+        rdf = ROOT.RDataFrame("Events", nanoAODs[s])
         
-        # only collect events w/ >1 muon and find muon pair closest to z mass
+        # only collect events w/ >1 muon and find muon pair closest to z mass. Muon1 is always charge -1 and muon2 always +1
         rdf = rdf.Filter("Muon_pt.size() > 1")
-        rdf = rdf.Define("ind", "ROOT::VecOps::RVec<Int_t> (get_indices(nMuon, &Muon_pt, &Muon_eta, &Muon_phi, &Muon_mass, &Muon_tkRelIso, &Muon_mediumId))")
+        rdf = rdf.Define("ind", "ROOT::VecOps::RVec<Int_t> (get_indices(nMuon, &Muon_pt, &Muon_eta, &Muon_phi, &Muon_mass, &Muon_tkRelIso, &Muon_mediumId, &Muon_charge))")
         rdf = rdf.Define("ind0", "ind[0]").Define("ind1", "ind[1]")
         rdf = rdf.Filter("ind0 + ind1 > 0")
         rdf = rdf.Define("pt_1", "Muon_pt[ind[0]]").Define("pt_2", "Muon_pt[ind[1]]")
         rdf = rdf.Define("mass_1", "Muon_mass[ind[0]]").Define("mass_2", "Muon_mass[ind[1]]")
         rdf = rdf.Define("eta_1", "Muon_eta[ind[0]]").Define("eta_2", "Muon_eta[ind[1]]")
         rdf = rdf.Define("phi_1", "Muon_phi[ind[0]]").Define("phi_2", "Muon_phi[ind[1]]")
+        rdf = rdf.Define("charge_1", "Muon_charge[ind[0]]").Define("charge_2", "Muon_charge[ind[1]]")
 
         # Define quantities of Z boson and collect those events with 50 < m_Z < 130
         rdf = rdf.Define("p4_1", "ROOT::Math::PtEtaPhiMVector(pt_1, eta_1, phi_1, mass_1)")
@@ -142,21 +150,20 @@ def make_ntuples(samples):
                 "genpt_2", "genmass_2", "geneta_2", "genphi_2"
             ]
         # make output with interesting data
-        rdf.Snapshot("Events", f"{datadir}/{s}_ntuple.root", quants)
+        rdf.Snapshot("Events", ntuples[s], quants)
 
 
-        
+ 
 # start of function to extract z pt distribution
-def hist_zpt(samples, pt_bins):
+def hist_zpt(ntuples, pt_bins):
     ROOT.gROOT.Reset()
     ROOT.gROOT.SetBatch()
     ROOT.gStyle.SetOptStat(0)
     hists = []
 
-    datadir = "/ceph/jdriesch/rochester/"
-    for s in samples[:2]:
+    for s in ntuples:
 
-        rdf = ROOT.RDataFrame("Events", f"{datadir}/{s}_ntuple.root")
+        rdf = ROOT.RDataFrame("Events", ntuples[s])
 
         h = rdf.Histo1D((f"h_Zboson_pt_{s}", "", len(pt_bins)-1, array('f', pt_bins)), "pt_Z")
         h.Scale(1./h.Integral())
@@ -169,15 +176,70 @@ def hist_zpt(samples, pt_bins):
 
 
 
+def hist_oneOverpT(ntuples, oneOverpT_bins, eta_bins, phi_bins):
+    ROOT.gROOT.ProcessLine('TFile* tf = TFile::Open("z_reweighting.root", "READ");')
+    ROOT.gROOT.ProcessLine('TH1D* h_dt = (TH1D*)tf->Get("h_Zboson_pt_DATA");')
+    ROOT.gROOT.ProcessLine('TH1D* h_mc = (TH1D*)tf->Get("h_Zboson_pt_MC");')
+    ROOT.gROOT.ProcessLine('TH1D* h_ratio = (TH1D*)h_dt->Clone();')
+    ROOT.gROOT.ProcessLine('h_ratio->Divide(h_mc)')
+    # ROOT.gROOT.ProcessLine('tf->Close()')
+    hists = []
+    for s in ntuples:
+        rdf = ROOT.RDataFrame("Events", ntuples[s])
+        rdf = rdf.Define("z_pT_weight", "h_ratio->GetBinContent(h_ratio->FindBin(pt_Z))")
+        rdf = rdf.Define("oneOverpT_1", "1./pt_1")
+        rdf = rdf.Define("oneOverpT_2", "1./pt_2")
+        h_neg = rdf.Histo3D(
+            (
+                f"h_oneOverpT_{s}_neg", "", 
+                len(eta_bins)-1, array('f', eta_bins), 
+                len(phi_bins)-1, array('f', phi_bins), 
+                len(oneOverpT_bins)-1, array('f', oneOverpT_bins)
+            ),
+            "eta_1",
+            "phi_1",
+            "oneOverpT_1",
+            "z_pT_weight"
+        )
+        hists.append(h_neg)
+
+        h_pos = rdf.Histo3D(
+            (
+                f"h_oneOverpT_{s}_pos", "",
+                len(eta_bins)-1, array('f', eta_bins), 
+                len(phi_bins)-1, array('f', phi_bins), 
+                len(oneOverpT_bins)-1, array('f', oneOverpT_bins)
+            ),
+            "eta_1",
+            "phi_1",
+            "oneOverpT_2",
+            "z_pT_weight"
+        )
+        hists.append(h_pos)
+    tf = ROOT.TFile("oneOverpT.root","RECREATE")
+    for h in hists:
+        h.Write()
+    tf.Close()
+
 if __name__=='__main__':
 
     pt_bins = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 100, 140, 200]
+    oneOverpT_bins = [i/200. for i in range(20)]
     eta_bins = [-2.4, -0.8, 0.8, 2.4]
     phi_bins = [-3.2, 0, 3.2]
 
     lumi = 31906.78
     xsec = 5600
-
-    samples = ['MC', 'DATA', 'GEN']
-    make_ntuples(samples)
-    hist_zpt(samples, pt_bins)
+    
+    datadir = "/ceph/jdriesch/rochester/"
+    nanoAODs = {
+        'MC': f"{datadir}MC.root",
+        'DATA': f"{datadir}DATA.root",
+    }
+    ntuples = {
+        'MC': f"{datadir}MC_ntuples.root",
+        'DATA': f"{datadir}DATA_ntuples.root",
+    }
+    # make_ntuples(nanoAODs, ntuples)
+    hist_zpt(ntuples, pt_bins)
+    hist_oneOverpT(ntuples, oneOverpT_bins, eta_bins, phi_bins)
