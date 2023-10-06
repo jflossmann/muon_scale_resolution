a10b78ee4371643cce3b20d86f33de1fd442c183
Dorian Guthmann <73468537+dguthmann98@users.noreply.github.com>
2023-10-06 15:14:03 +0200

    Update zmass.py: added comments

--- python/zmass.py
+++ python/zmass.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 from multiprocessing import Pool, RLock
 import os
 
+#Dictionary for names and variables of histograms
 plotdict = {
     'MC': {
         'h_EtaPhiVsMz_neg_MC': ['eta_1', 'phi_1', 'mass_Z'],
@@ -32,27 +33,33 @@ plotdict = {
 
 
 def roofit_mass(hist, e, p):
+    """fits a Voigt function to a given histogram and returns result and errors"""
     ROOT.RooMsgService.instance().setSilentMode(True)
     ROOT.RooMsgService.instance().setGlobalKillBelow(ROOT.RooFit.ERROR)
 
     results, errors, plots = {}, {}, {}
 
+    #definition of mass-variable
     x = ROOT.RooRealVar("x", "m_vis (GeV)", 85, 97)
     x.setBins(10000,"cache")
     x.setMin("cache",0)
     x.setMax("cache",500)
 
+    #definition of fit parameters
     Z_mass = ROOT.RooRealVar("Z_mass", "Z_mass", 91.1876, 60, 120)
     Z_width = ROOT.RooRealVar("Z_width", "Z_width", 2.4952, 2.4952, 2.4952)
     Z_width.setConstant(True)
     sigma = ROOT.RooRealVar("sigma", "sigma", 0.1, 0, 10)
     n_sigma=1
 
+    #creation of voigt-model
     func = ROOT.RooVoigtian("vgt_mc", "Voigt", x, Z_mass, Z_width, sigma)
 
+    #creation of a RooDataHist from histogram
     roohist = ROOT.RooDataHist('h', '', ROOT.RooArgSet(x), hist)
+    #perform fit
     fitResult = func.fitTo(roohist, ROOT.RooFit.AsymptoticError(True), ROOT.RooFit.PrintEvalErrors(-1))
-
+    
     results = [Z_mass.getVal(), n_sigma*sigma.getVal()]
     errors = [Z_mass.getAsymErrorHi(), n_sigma*sigma.getAsymErrorHi()]
 
@@ -60,13 +67,15 @@ def roofit_mass(hist, e, p):
 
 
 
-def hist_zmass(ntuples, eta_bins, phi_bins, mass_bins, hdir):
+def hist_zmass(ntuples, eta_bins, phi_bins, mass_bins, hdir)->None:
+    """produces 3D and 1D histograms from given ntuples"""
     hists_3d = []
     hists_1d = []
     for s in ntuples:
         rdf = ROOT.RDataFrame("Events", ntuples[s])
 
         for n in plotdict[s]:
+            #create 3D histogram from ntuple
             hists_3d.append(
                 rdf.Histo3D(
                     (
@@ -93,6 +102,7 @@ def hist_zmass(ntuples, eta_bins, phi_bins, mass_bins, hdir):
 
 
 def fit_zmass(eta_bins, phi_bins, hdir):
+    """performs fit for z-mass histogram. produces a new histogram for the fit results"""
     hists = []
 
     tf = ROOT.TFile(f'{hdir}zmass.root', 'read')
@@ -132,6 +142,7 @@ def job_wrapper(args):
 
 
 def plot_zmass(eta_bins, phi_bins, hdir):
+    """produces plots for fit results and saves them in "plots/zmass_fits/" """
     ROOT.gROOT.SetBatch(1)
     tf = ROOT.TFile(f'{hdir}zmass_fitresults.root', 'read')
     os.makedirs(f'plots/zmass_fits/', exist_ok=True)
