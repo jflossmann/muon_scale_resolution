20316034d4fef4c41b2fb4d43ed5bef51936c805
jdriesch <jost.driesch@kit.edu>
2023-10-06 11:27:03 +0200

    changed mass binning for hists and fit, added zmass plotting

--- rochester_rdf.py
+++ rochester_rdf.py
@@ -9,6 +9,7 @@ import os
 import python.ntuple as ntuple
 import python.scale_corr as corr
 import python.plot as plot
+import python.zmass as zmass
 
 def parse_args():
     parser = ArgumentParser(
@@ -38,6 +39,13 @@ def parse_args():
         action='store_true',
         help='Make plots'
     )
+    parser.add_argument(
+        '-Z',
+        '--zmass',
+        default=False,
+        action='store_true',
+        help='Check Z mass'
+    )
     args = parser.parse_args()
     return args
 
@@ -46,10 +54,11 @@ def parse_args():
 if __name__=='__main__':
 
     pt_bins = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 100, 200]
-    oneOverPt_bins = np.linspace(1/200,1/20, 1000)#[i/2000000. for i in range(200000)]
+    oneOverPt_bins = np.linspace(1/200,1/20, 200)#[i/2000000. for i in range(200000)]
     eta_bins = [-2.4, -2.1, -1.85, -0.4, 0, 0.4, 1.85, 2.1, 2.4]
     phi_bins = np.linspace(-3.2, 3.2, 17) #[-3.2, -2.4, -1.6, -.8, 0, .8, 1.6, 2.4, 3.2]
     charge_bins = [-2,0,2]
+    mass_bins = np.linspace(75, 105, 61)
 
     lumi = 31906.78
     xsec = 5600
@@ -77,15 +86,19 @@ if __name__=='__main__':
 
     if args.scale:
         os.makedirs(hdir, exist_ok=True)
-        corr.hist_oneOverpT(ntuples, oneOverPt_bins, eta_bins, phi_bins, hdir)
+        corr.hist_oneOverpT(ntuples, oneOverPt_bins, eta_bins, phi_bins, hdir, pdir)
         corr.get_scale_corrections(ntuples, eta_bins, phi_bins, charge_bins, hdir)
         corr.apply_scale_corrections(ntuples, eta_bins, phi_bins, charge_bins, hdir)
-        corr.scale_closure(ntuples, oneOverPt_bins, eta_bins, phi_bins, hdir)
-    
+        corr.hist_oneOverpT(ntuples_corr, oneOverPt_bins, eta_bins, phi_bins, hdir, pdir, corr='_peak_roccor')
 
     if args.plot:
         os.makedirs(pdir, exist_ok=True)
-        plot.hist_ntuples(ntuples_corr, "mass_Z", 90, 60, 120, hdir, "mass_z", "RECREATE")
-        plot.hist_ntuples(ntuples_corr, "mass_Z_roccor", 90, 60, 120, hdir, "mass_z")
+        plot.hist_ntuples(ntuples_corr, "mass_Z", len(mass_bins)-1, mass_bins[0], mass_bins[-1], hdir, "mass_z", "RECREATE")
+        for corr in ['_mean_roccor', '_median_roccor', '_peak_roccor']:
+            plot.hist_ntuples(ntuples_corr, "mass_Z"+corr, len(mass_bins)-1, mass_bins[0], mass_bins[-1], hdir, "mass_z")
         plot.plot_stuff(pdir, eta_bins, phi_bins)
         
+    if args.zmass:
+        zmass.hist_zmass(ntuples_corr, eta_bins, phi_bins, mass_bins, hdir)
+        zmass.fit_zmass(eta_bins, phi_bins, hdir)
+        zmass.plot_zmass(eta_bins, phi_bins, hdir)
