4d283dfce98206df4e8e636d5f98e542ed8cffa5
jdriesch <jost.driesch@kit.edu>
2023-10-15 20:22:27 +0200

    added scale factors

--- env.sh
+++ env.sh
@@ -1,1 +1,8 @@
+echo "sourcing LCG stack"
 source /cvmfs/sft.cern.ch/lcg/views/LCG_102rc1/x86_64-centos7-gcc11-opt/setup.sh
+
+SFDIR=./data/scaleFactors
+if [ ! -d $SFDIR ]; then
+    echo "get muon efficiency scale factor repo"
+    git clone https://gitlab.cern.ch/cms-muonPOG/muonefficiencies.git ./data/scaleFactors
+fi
