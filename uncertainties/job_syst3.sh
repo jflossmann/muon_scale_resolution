e999037ef7507d4b38f5fa1bc0db298279dcd166
jdriesch <jost.driesch@kit.edu>
2024-03-02 11:18:14 +0100

    systematics job submission

--- uncertainties/job_syst3.sh
+++ uncertainties/job_syst3.sh
@@ -3,4 +3,4 @@
 cd /work/jdriesch/CMS_service/rochester/new_rochester/rochester_shire_source
 
 source env.sh
-python rochester_rdf.py -4 --process $1
+python rochester_rdf_syst.py -1 -2 -3 -4 --process $1 --syst 3
